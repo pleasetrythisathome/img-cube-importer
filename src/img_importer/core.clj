dad4d53e9a70427009d1ce0a1d43536e8c6db118
Dylan Butman <dbutman@nyu.edu>
2014-06-20 17:53:39 -0400

    refactor. save output json with new paths

--- src/img_importer/core.clj
+++ src/img_importer/core.clj
@@ -8,26 +8,39 @@
 
 (def cubes (parse-string (slurp (io/resource "cubes.json"))))
 
-(defn indexed [f v]
+(defn map-iv [f v]
   (let [idv (map vector (iterate inc 0) v)]
-    (doseq [[index value] idv]
-      (f value index))))
+    (for [[index value] idv]
+      (f index value))))
 
 (defn curl-save [uri file]
-  (pprint (str "saving " uri " to " file))
   (sh "curl" "-L" "-q" "-s" uri "-o" file))
 
-(doseq [{:strs [images title]} cubes]
-  (sh "mkdir" "resources/cubes")
-  (let [path (s/replace (s/lower-case title) #" " "_")]
-    (sh "rm" "-rf" (str "resources/cubes/" path))
-    (sh "mkdir" (str "resources/cubes/" path))
-    (indexed (fn [img i]
-               (let [fname (str "resources/cubes/" path "/" i)]
-                 (curl-save img fname)
-                 (let [ext (-> fname
-                               (io/file)
-                               pm/mime-type-of
-                               pm/extension-for-name)]
-                   (sh "mv" fname (str fname ext)))))
-             images)))
+(defn rename-with-ext [file]
+  (let [ext (-> file
+                (io/file)
+                pm/mime-type-of
+                pm/extension-for-name)
+        with-ext (str file ext)]
+    (sh "mv" file with-ext)
+    with-ext))
+
+(defn save-images [images path]
+  (into [] (map-iv (fn [i img]
+                     (let [fname (str path "/" i)]
+                       (curl-save img fname)
+                       (rename-with-ext fname)))
+                   images)))
+
+(defn save-cubes [cubes path]
+  (sh "mkdir" path)
+  (for [{:strs [images title] :as cube} cubes]
+    (let [key (s/replace (s/lower-case title) #" " "_")
+          cube-path (str path "/" key)]
+      (sh "rm" "-rf" cube-path)
+      (sh "mkdir" cube-path)
+      (assoc cube "images" (save-images images cube-path)))))
+
+(let [saved (save-cubes cubes "/Users/HereNow/code/ib5k/fakelove/cubes/cl-cuber/cl-chrome-imagecube/core/static/googleIO/images/cubes")]
+  (pprint saved)
+  (spit "/Users/HereNow/code/ib5k/fakelove/cubes/cl-cuber/cl-chrome-imagecube/core/static/googleIO/scripts/cubes.json" (generate-string saved)))
